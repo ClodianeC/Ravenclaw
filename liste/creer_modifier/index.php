334a839a3a2f11b982c85f5c3c52fe72b3138733
Clodiane Charette <2164515@csfoy.ca>
2022-11-30 16:47:02 -0500

    HTML statique, ajout favicon, modif head.html->head.php

--- liste/creer_modifier/index.php
+++ liste/creer_modifier/index.php
@@ -1,1 +1,78 @@
 <?php
+$niveau = "../../";
+
+include($niveau."inc/config.inc.php");
+
+$action = "Ajouter";
+$nomDeListe = "Liste quelconque"
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoList - <?php echo $action." ".$nomDeListe ?></title>
+    <?php include($niveau."inc/fragments/head.php"); ?>
+    <link rel="stylesheet" href="<?php echo $niveau ?>css/styles_clodiane.css">
+</head>
+<main>
+    <?php include($niveau."inc/fragments/entete.html"); ?>
+    <a href="../index.php"><div class="icon" id="retourListe">Retour à la liste</div></a>
+    <h1 class="h1">Modifier un item</h1>
+    <h1 class="h1">Nouvel item</h1>
+    <h2 class="h2">Pour <?php echo $nomDeListe ?></h2>
+    <p class="avertissement">Les champs accompagnés d'un astérisque (*) sont obligatoires</p>
+    <form action="./index.php" method="get">
+        <fieldset>
+            <legend>Couleur de l'item</legend>
+            <label for="aleatoire">Aléatoire</label>
+            <input type="radio" name="couleur" id="aleatoire" value="aleatoire">
+            <label for="blanc">Blanc</label>
+            <input type="radio" name="couleur" id="blanc" value="blanc">
+            <label for="tPale">Gris très pâle</label>
+            <input type="radio" name="couleur" id="tPale" value="tPale">
+            <label for="pale">Gris pâle</label>
+            <input type="radio" name="couleur" id="pale" value="pale">
+            <label for="moyen">Gris moyen</label>
+            <input type="radio" name="couleur" id="moyen" value="moyen">
+            <label for="fonce">Gris foncé</label>
+            <input type="radio" name="couleur" id="fonce" value="fonce">
+            <label for="tFonce">Gris très foncé</label>
+            <input type="radio" name="couleur" id="tFonce" value="tFonce">
+        </fieldset>
+
+        <label for="nom">Nom de l'item</label>
+        <input type="text" name="nom" id="nom" value="">
+        <fieldset name="date">
+            <legend>Date due</legend>
+            <select id="jour" name="jour">
+                <option value="0" selected></option>
+                <option value="01">01</option>
+            </select>
+            <select id="mois" name="mois">
+                <option value="0" selected></option>
+                <option value="01">Janvier</option>
+            </select>
+            <select id="annee" name="annee">
+                <option value="0" selected></option>
+                <option value="01">2022</option>
+            </select>
+
+        </fieldset>
+        <label for="char01">Charactéristique 01</label>
+        <input type="text" name="char01" id="char01" value="">
+        <label for="char02">Charactéristique 02</label>
+        <input type="text" name="char02" id="char02" value="">
+        <label for="char03">Charactéristique 03</label>
+        <input type="text" name="char03" id="char03" value="">
+        <button type="submit" name="modifier" id="modifier" value="modifier">Modifier l'item</button>
+        <a href="../index.php">Annuler</a>
+    </form>
+</main>
+<footer>
+    <?php include($niveau."inc/fragments/footer.html"); ?>
+</footer>
+</html>
--- liste/index.php
+++ liste/index.php
@@ -1,1 +1,47 @@
 <?php
+$niveau = "../";
+
+include($niveau."inc/config.inc.php");
+
+$nomDeListe = "Liste quelconque";
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoList - <?php echo $nomDeListe ?></title>
+    <?php include($niveau."inc/fragments/head.php"); ?>
+    <link rel="stylesheet" href="<?php echo $niveau ?>css/styles_clodiane.css">
+</head>
+<main>
+    <?php include($niveau."inc/fragments/entete.html"); ?>
+    <a href="../index.php"><div class="icon" id="retourAccueil">Retour à l'accueil</div></a>
+    <h1 class="h1"><?php echo $nomDeListe?></h1>
+    <form action="creer_modifier/index.php" method="get">
+        <button type="submit" name="add" id="add" value="add"></div>Ajouter un item</button>
+    </form>
+    <ul>
+        <li>
+            <h2 class="h2">Nom de l'item</h2>
+            <p>Date due: 12 nov 2022</p>
+            <p>Charactéristique 01: Rond</p>
+            <p>Charactéristique 02: Bleu</p>
+            <p>Charactéristique 03: Karaté</p>
+            <p>Actif</p>
+            <p hidden>Complété</p>
+            <input type="range" name="actif_complete" id="actif_complete" min="0" max="1">
+            <form action="creer_modifier/index.php" method="get">
+                <button type="submit" name="edit" id="edit" value="edit"><div class="icon"></div>Éditer</button>
+                <button type="submit" name="delete" id="delete" value="delete"><div class="icon"></div>Supprimer</button>
+            </form>
+        </li>
+    </ul>
+</main>
+<footer>
+    <?php include($niveau."inc/fragments/footer.html"); ?>
+</footer>
+</html>
