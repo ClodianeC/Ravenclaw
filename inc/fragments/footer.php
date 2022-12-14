f2471841cf80043d2badd55e17ced44d6d54576d
Clodiane Charette <2164515@csfoy.ca>
2022-12-14 17:42:54 -0500

    CSS fini pour footer

--- inc/fragments/footer.php
+++ inc/fragments/footer.php
@@ -4,13 +4,15 @@
 </head>
 <div class="footer">
     <p class="inspiration">Vous manquez d'inspiration? Voici des listes poppulaires auprès de nos utilisateurs: </p>
-    <ul class="liste-inspi">
-        <li class="inspi">Liste d'épicerie</li>
-        <li class="inspi">Cadeaux de Noël</li>
-        <li class="inspi">Listes de blagues</li>
-        <li class="inspi">Tâches à faire</li>
-        <li class="inspi">Sites web inspirants</li>
-    </ul>
-    <img src="<?php echo $niveau ?>img/images/logo-footer.jpg" class="img_footer">
+    <div class="infos_footer">
+        <ul class="liste-inspi">
+            <li class="inspi">Liste d'épicerie</li>
+            <li class="inspi">Cadeaux de Noël</li>
+            <li class="inspi">Listes de blagues</li>
+            <li class="inspi">Tâches à faire</li>
+            <li class="inspi">Sites web inspirants</li>
+        </ul>
+        <img src="<?php echo $niveau ?>img/images/logo-footer.jpg" class="img_footer">
+    </div>
     <p class="droits">TodoList 2022 | Tous droits réservés<br>Réalisé dans le cadre du cours RPNI au Cégep de Sainte-Foy</p>
 </div>
